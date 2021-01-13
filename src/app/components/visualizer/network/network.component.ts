000132af9d9ad4ad9876165031dfbaa7edb9a0b9
Viktor Uhryn <vik.ugrin@gmail.com>
2021-01-13 05:19:19 +0200

    fix: issue with fitting network (#93)

--- src/app/components/visualizer/network/network.component.ts
+++ src/app/components/visualizer/network/network.component.ts
@@ -29,6 +29,8 @@ export class NetworkComponent implements OnDestroy {
         scale,
         position
       });
+    } else {
+      this._instance.fit();
     }
 
     this._exportToImage.enable({
